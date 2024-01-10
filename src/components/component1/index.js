9bb4c719384cdba0abd5454cead15c53d74487e3
anjeelaali <anjeelaali6@gmail.con>
2024-01-10 15:33:46 +0500

    work completed.

--- src/components/component1/index.js
+++ src/components/component1/index.js
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useContext } from 'react'
 import Context from '../../context/usecontext'
 
 const ComponentOne = () => {
   const a = useContext(Context)
+
+  useEffect(()=>{
+      a.update();
+  }, [])
   return (
     <>
-
-    <h1>
+      <h1>
         This is component One
-    </h1>
-    <h2>
-      Hi,{a.name} !
-    </h2>
-      
+      </h1>
+      <h2>
+        Hi,i'm {a.state.name} ! and i am in class {a.state.class}
+      </h2>
+
     </>
   )
 }
--- src/context/state.js
+++ src/context/state.js
@@ -4,13 +4,25 @@ import Context from "./usecontext";
 
 const ContextState = (props) => {
 
-    const state = {
+    const state_1= {
         name: 'anjeela',
         class: '2nd Year'
     }
 
+    const [state, setState] = useState(state_1);
+
+    const update =()=>{
+        setTimeout(()=>{
+            setState({
+                name: 'ali',
+            class: '1st Year'
+            })
+        }, 1000)
+       
+    }
+
     return (
-        <Context.Provider value={state}>
+        <Context.Provider value={{state, update}}>
             {props.children}
         </Context.Provider>
     )
