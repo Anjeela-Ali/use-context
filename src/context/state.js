1739913fec418517c813b0f6bba88018a120fd3e
anjeelaali <anjeelaali6@gmail.con>
2024-01-11 11:58:39 +0500

    Added comments.

--- src/context/state.js
+++ src/context/state.js
@@ -2,15 +2,19 @@ import { useState } from "react";
 import Context from "./usecontext";
 
 
-const ContextState = (props) => {
 
+// Component for  managing states.
+const ContextState = (props) => {
+// initial state.
     const state_1= {
         name: 'anjeela',
         class: '2nd Year'
     }
-
+// State that is going to be update.
     const [state, setState] = useState(state_1);
 
+
+    // Function for updating the state.
     const update =()=>{
         setTimeout(()=>{
             setState({
@@ -22,6 +26,8 @@ const ContextState = (props) => {
     }
 
     return (
+
+        // we wrap the childern within the Context so that we could used state or update our state in our project any where.
         <Context.Provider value={{state, update}}>
             {props.children}
         </Context.Provider>
